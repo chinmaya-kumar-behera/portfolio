43e03fd8fe3741c7902b549b397f36c0011e88b2
Chinmaya Kumar Behera <official.chinu2127@gmail.com>
2024-02-05 02:05:00 +0530

    Update layout.js

--- web/app/layout.js
+++ web/app/layout.js
@@ -11,6 +11,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <title>My Portfolio Site</title>
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   );
