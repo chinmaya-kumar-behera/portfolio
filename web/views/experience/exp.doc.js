f0c24a4d52a826255c4a9fefb6a02f4629445920
chinmaya-kumar-behera <official.chinu2127@gmail.com>
2024-07-13 03:04:49 +0530

    Update exp.doc.js

--- web/views/experience/exp.doc.js
+++ web/views/experience/exp.doc.js
@@ -9,15 +9,19 @@ import eight from "../../_asssets/images/projects/learnduke/ld_sample_8.png";
 import nine from "../../_asssets/images/projects/learnduke/ld_sample_9.png";
 
 export const Exp = {
-  role: "Web Full Stack Developer",
+  role: "Full Stack Developer at Learnduke",
   description: [
     "Assisted with creating highly interactive and reusable web components using the JavaScript library <strong>React.js</strong>.",
     "Proficiency in frameworks including <strong>Tailwind CSS</strong>, <strong>Material Tailwind</strong>, and <strong>Material UI</strong> for streamlined web development.",
+    "Utilized <strong>React-Cropper</strong>library for image cropping functionality and collaborated with multiple React libraries to seamlessly integrate <strong>Rich text editor</strong> and <strong>PDF viewer</strong> into the web application.",
+    "Developed a <strong>customizable Creatable Select component</strong> from scratch to facilitate seamless selection processes, dynamically generating options if the searched term is not found in the database.",
+    "Designed a <strong>Profile Completeness Feature</strong> to prompt users for missing details, enhancing engagement and completion. Implemented a <strong>progressive image loading technique</strong>, which helped boost site speed by 30%.",
     "Utilized <strong>Recoil</strong> for state management in React, enabling efficient and scalable management of application state.",
     "Applied <strong>Node.js</strong> utility library Express to facilitate streamlined server-side operations.",
     "Proficient in designing and implementing <strong>RESTful APIs</strong> to ensure robust communication between clients and servers.",
     "Implemented <strong>caching</strong> techniques to improve backend performance by efficiently storing and fetching user data, resulting in fewer database queries and faster system responsiveness.",
     "Utilized <strong>Mongoose</strong> for writing CRUD operations, facilitating seamless data retrieval and updating in MongoDB databases.",
+    "Actively contributed to in-app notifications using WebSocket and payment integration, enhancing real-time interaction and transaction reliability",
   ],
 };
 
